394c79444e72273331876da04f39b2d23bf182d6
Amin.A <amin@goquo.com>
2018-07-07 12:15:45 +0800

    init commit.

---
+++ KerasPredict.cpp
@@ -0,0 +1,197 @@
+#include <napi.h>
+#include <fdeep/fdeep.hpp>
+
+class ErrorAsyncWorker : public Napi::AsyncWorker
+{
+  public:
+    ErrorAsyncWorker(
+        const Napi::Function &callback,
+        Napi::Error error) : Napi::AsyncWorker(callback), error(error)
+    {
+    }
+
+  protected:
+    void Execute() override
+    {
+        // Do nothing...?
+    }
+
+    void OnOK() override
+    {
+        Napi::Env env = Env();
+
+        Callback().MakeCallback(
+            Receiver().Value(), {error.Value(),
+                                 env.Undefined()});
+    }
+
+    void OnError(const Napi::Error &e) override
+    {
+        Napi::Env env = Env();
+
+        Callback().MakeCallback(
+            Receiver().Value(), {e.Value(),
+                                 env.Undefined()});
+    }
+
+  private:
+    Napi::Error error;
+};
+
+class PredicateAsyncWorker : public Napi::AsyncWorker
+{
+  public:
+    PredicateAsyncWorker(const Napi::Function &callback,
+                         int **testCases, int testCaseCount,
+                         int footPrintSize, std::string mlModelPath)
+        : Napi::AsyncWorker(callback), testCases(testCases), testCaseCount(testCaseCount),
+          footPrintSize(footPrintSize), mlModelPath(mlModelPath)
+    {
+    }
+
+  protected:
+    void Execute() override
+    {
+        const auto model = fdeep::load_model(this->mlModelPath);
+        this->results = new double[this->testCaseCount];
+        fdeep::shape3 tensor3_shape(this->footPrintSize, 1, 1);
+        for (int x = 0; x < this->testCaseCount; ++x)
+        {
+            fdeep::tensor3 t(tensor3_shape, 0.0f);
+            for (int y = 0; y < this->footPrintSize; ++y)
+            {
+                t.set(y, 1, 1, this->testCases[x][y]);
+            }
+            const auto result = model.predict({t});
+            fdeep::internal::tensor3_pos pos = fdeep::internal::tensor3_pos(0, 0, 0);
+            this->results[x] = result[0].get(pos);
+        }
+    }
+
+    void OnOK() override
+    {
+        Napi::Env env = Env();
+        Napi::Array res = Napi::Array::New(env, this->testCaseCount);
+        Napi::Number tmp;
+        for (int i = 0; i < this->testCaseCount; i++)
+        {
+            res[i] = Napi::Number::New(env, this->results[i]);
+        }
+        Callback().MakeCallback(
+            Receiver().Value(), {env.Null(), res});
+    }
+
+    void OnError(const Napi::Error &e) override
+    {
+        Napi::Env env = Env();
+        Callback().MakeCallback(
+            Receiver().Value(), {e.Value(),
+                                 env.Undefined()});
+    }
+
+  private:
+    int footPrintSize;
+    std::string mlModelPath;
+    int **testCases;
+    int testCaseCount;
+    double *results;
+};
+
+void PredictAsyncCallback(const Napi::CallbackInfo &info)
+{
+    Napi::Env env = info.Env();
+
+    //
+    // Account for known potential issues that MUST be handled by
+    // synchronously throwing an `Error`
+    //
+    if (info.Length() < 4)
+    {
+        Napi::TypeError::New(env, "Invalid argument count").ThrowAsJavaScriptException();
+        return;
+    }
+    if (!info[3].IsFunction())
+    {
+        Napi::TypeError::New(env, "Invalid argument types").ThrowAsJavaScriptException();
+        return;
+    }
+
+    //
+    // Handle all other potential issues asynchronously via the provided callback
+    //
+
+    Napi::Function cb = info[3].As<Napi::Function>();
+    if (!info[0].IsNumber())
+    {
+        (new ErrorAsyncWorker(cb, Napi::TypeError::New(env, "Invalid argument types")))->Queue();
+    }
+    else if (!info[1].IsString())
+    {
+        (new ErrorAsyncWorker(cb, Napi::TypeError::New(env, "Invalid argument types")))->Queue();
+    }
+    else if (!info[2].IsArray())
+    {
+        (new ErrorAsyncWorker(cb, Napi::TypeError::New(env, "Invalid argument types")))->Queue();
+    }
+    else
+    {
+        // ml config
+        // testCaseSize
+        int footPrintSize = (int32_t)Napi::Number(env, info[0]);
+        // ML model path
+        std::string mlModelPath = std::string(Napi::String(env, info[1]));
+
+        Napi::Array input = Napi::Array(env, info[2]);
+        int testCaseCount = input.Length();
+        int **testCases = new int *[testCaseCount];
+        for (int i = 0; i < (int)testCaseCount; ++i)
+        {
+            testCases[i] = new int[footPrintSize];
+            napi_value part;
+            napi_get_element(env, info[2], i, &part);
+
+            bool isarray;
+            napi_is_array(env, part, &isarray);
+
+            if (!isarray)
+            {
+                (new ErrorAsyncWorker(cb, Napi::TypeError::New(env, "Invalid test case.")))->Queue();
+            }
+            else
+            {
+                Napi::Array testCase = Napi::Array(env, part);
+                if (testCase.Length() != footPrintSize)
+                {
+                    (new ErrorAsyncWorker(cb, Napi::TypeError::New(env,
+                                                                   "Invalid test case. test case size should be as foot print size.")))
+                        ->Queue();
+                    return;
+                }
+                else
+                {
+                    for (int j = 0; j < footPrintSize; j++)
+                    {
+                        napi_value val;
+                        int target;
+                        napi_get_element(env, testCase, j, &val);
+                        napi_get_value_int32(env, val, &target);
+                        testCases[i][j] = target;
+                    }
+                }
+            }
+        }
+        (new PredicateAsyncWorker(cb, testCases, testCaseCount, footPrintSize, mlModelPath))->Queue();
+    }
+    return;
+}
+
+// init nodejs module.
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+    exports.Set(
+        Napi::String::New(env, "predict"),
+        Napi::Function::New(env, PredictAsyncCallback));
+    return exports;
+}
+
+NODE_API_MODULE(predict, Init)
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # keras-predict
 A Nodejs Native addons to run Keras ML model.
+
+ Run Keras ML model via [frugally-deep](https://github.com/Dobiasd/frugally-deep) as a native Nodejs Addons.
+ 
+ usage: 
+ * first [install](https://github.com/Dobiasd/frugally-deep/blob/master/INSTALL.md) frugally-deep.
+ * compile your keras model :
+ ```
+python3 frugally-deep/keras_export/convert_model.py keras_model.h5 fdeep_model.json
+ ```
+ * path the size of your ML input and converted ML path to predict function.
+ ```
+const predict = require('keras-predict')
+const path = require('path');
+const mlModelPath = path.resolve(__dirname, "fdeep_model.json");
+const testCase = [...];
+predict.predict(120, mlModelPath, testCase, (err, predict) => {
+        if (err) {
+            console.error(err)
+        } else {
+            console.dir(predict.map(Math.round))
+        }
+    })
+}
+ ```
+ 
+ * A working example avialable at example folder.
+
+Amin Aghabeiki.
